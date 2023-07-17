7a5b5024d43d5fa2a4b665a5f2d743318b7db679
IvanLabza <100775210+IvanLabza@users.noreply.github.com>
2023-07-17 13:02:07 +0300

    main

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const fetchObjects = async () => {
-    const response = await axios.get(URL);
-    const results = await response;
-    return results;
-  };
-
-  export{fetchObjects}
